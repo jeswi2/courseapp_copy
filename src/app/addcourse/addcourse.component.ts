83f3e95d15adb1db0bbb7b5c6f6e766d870c181c
jeswi2 <jeswimathewm@gmail.com>
2022-10-13 17:35:13 +0530

    fo

--- src/app/addcourse/addcourse.component.ts
+++ src/app/addcourse/addcourse.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-addcourse',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddcourseComponent implements OnInit {
 
-  constructor() { }
+  constructor(private myapi:ApiService) { }
 
   courseTitle=""
   courseDescription=""
@@ -24,6 +25,12 @@ export class AddcourseComponent implements OnInit {
   "courseVenue":this.courseVenue
   }
   console.log(data)
+  this.myapi.addCourse(data).subscribe(
+    (response)=>{
+      console.log(response)
+      alert("successfully added")
+    }
+  )
   }
 
 
